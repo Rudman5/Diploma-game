537bf78567d0d932978606d8920f42b5acf0a91d
Andraž Rudman <rudman.andraz335@gmail.com>
2025-07-07 19:10:59 +0200

    Added base name to config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(({ command, mode }) => {
   return {
+    base: '/Diploma-game/',
     resolve: {
       alias: {
         babylonjs: mode === 'development' ? 'babylonjs/babylon.max' : 'babylonjs',
