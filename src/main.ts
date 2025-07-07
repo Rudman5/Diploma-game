6250b9c4df97cf9e9b861a2be493ffa7753b8a34
Andraž Rudman <rudman.andraz335@gmail.com>
2025-07-07 18:58:22 +0200

    Remove Cesium, organize structure, import first ground material

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
-import { AppOne as App } from './AppOne';
+import { AppOne as App } from './core/AppOne';
 
-console.log(`main.ts starting ${App.name}`);
 window.addEventListener('DOMContentLoaded', () => {
   const canvas = document.getElementById('renderCanvas') as HTMLCanvasElement;
   const app = new App(canvas);
