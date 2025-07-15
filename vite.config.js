7fcb6638c9ffc29318a1ce8b489e06cbe2e8905f
Andraž Rudman <rudman.andraz335@gmail.com>
2025-07-15 19:06:49 +0200

    Added back base url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(({ command, mode }) => {
   return {
+    base: '/Diploma-game/',
     resolve: {
       alias: {
         babylonjs: mode === 'development' ? 'babylonjs/babylon.max' : 'babylonjs',
